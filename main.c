3424983a7ea85aa7409a87a54e5e94c71f38ab11
p0l0satik <denis.cucu@yandex.ru>
2021-07-29 12:05:00 +0300

    working buttons, setting ttime

--- main.c
+++ main.c
@@ -60,8 +60,15 @@ ISR(TIMER0_COMPA_vect){
     }
 }
 
+//MODES
+enum modes{
+    NORMAL    = 0,
+    SETTIME   = 1
+};
+uint8_t modes = 0;
+
 //digit state machine init
-uint8_t digits[6] = {1, 8, 1, 0, 0, 0};
+uint8_t digits[6] = {1, 4, 1, 4, 0, 0};
 enum digits{
     FIRST  = 0b00000001,
     SIXS   = 0b00100000
@@ -111,6 +118,7 @@ void upd_time(){
     digits[0] = (hr & 0xF0) >> 4 ;
     TWIStop();
 }
+
 void set_time(){
     TWIStart();
     TWISend(0b10100000);
@@ -133,11 +141,95 @@ void update_time(){
     } 
 }
 
+uint64_t next_button_update;
+uint8_t buttons = 0;
+#define time_but_upd 10
+//PIN == 0 when button pushed
+#define KEY_A                (1<<6) // red button,
+uint8_t prev_A = 0, count_A = 0;
+//PIN == 0 when button released
+#define KEY_B                (1<<0) //green button, PIN is correct
+uint8_t prev_B = 0, count_B = 0;
+
+uint8_t set_pair = 2;
+void upd_buttons(){
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+        if (next_button_update > time) return;
+        next_button_update = time + time_but_upd;
+    }
+    if ((PINB & KEY_B) == prev_B){
+        if (prev_B){
+            count_B++;
+        }
+
+        if (!prev_B && count_B > 0 && count_B < 30 && modes == NORMAL){
+            set_pair = 2;
+            modes = SETTIME;
+        }
+
+        if (count_B > 200 && modes == SETTIME){
+            modes = NORMAL;
+            set_time();
+        }
+
+        if (!prev_B && count_B > 0 && count_B < 30 && modes == SETTIME){
+            uint8_t set_val = digits[set_pair * 2] * 10 + digits[set_pair * 2 + 1];
+            set_val += 1;
+            set_val %= 60;
+            digits[set_pair * 2] = set_val / 10;
+            digits[set_pair * 2 + 1] = set_val % 10;
+        }
+
+        if (!prev_B) {
+            count_B = 0;
+        }
+    } else {
+        prev_B = (PINB & KEY_B);
+    }
+
+    if ((PINB & KEY_A) == prev_A){
+        if (!prev_A){
+            count_A++;
+        }
+
+        if (prev_A && count_A > 0 && count_A < 30 && modes == SETTIME){
+            uint8_t set_val = digits[set_pair * 2] * 10 + digits[set_pair * 2 + 1];
+            if (set_val){
+                set_val -= 1;
+            } else {
+                set_val = 59;
+            }
+            digits[set_pair * 2] = set_val / 10;
+            digits[set_pair * 2 + 1] = set_val % 10;
+        }
+        if (count_A > 200 && modes == SETTIME){
+            if (set_pair){
+                set_pair -= 1;
+            } else {
+                set_pair = 2;
+            }
+        }
+
+        if (prev_A) {
+            count_A = 0;
+        }
+    } else {
+        prev_A = (PINB & KEY_A);
+    }
+}
+
+
 int main(void){
     DDRD =  0b11111111;
     DDRC =  0b11111111;
+    //right button settings
+    DDRB =  0;
+    PORTB = 0b11111111;
+    //
+    // PORTC = digits_convert[0];
+    // PORTD = 0b00000011;
     TWIInit();
-    // set_time();
+    set_time();
 
     //Timer init
     cli();
@@ -149,8 +241,11 @@ int main(void){
     sei();
     digite_state_m_init();
     while(1){
+        upd_buttons();
         change_digit();
-        update_time();
+        if (modes == NORMAL){
+            update_time();
+        }
     }
     return 0;
 }
